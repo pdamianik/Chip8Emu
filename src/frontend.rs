3e3c7ea0960d3cd6c97d9673f1aaee2a20ebdcfd
Philip Damianik <39028343+pdamianik@users.noreply.github.com>
2021-06-30 16:25:02 +0200

    Add LICENSE

--- src/frontend.rs
+++ src/frontend.rs
@@ -1,4 +1,22 @@
-use std::{sync::{Arc, Mutex, mpsc::{Receiver, Sender}}};
+/*
+    Chip8Emu - a Chip-8 emulator
+    Copyright (C) 2021  Philip Damianik
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ use std::{sync::{Arc, Mutex, mpsc::{Receiver, Sender}}};
 
 use crate::emu::display::Change;
 
